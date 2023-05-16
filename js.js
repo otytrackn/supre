f616a367766ba6ce4a65e0e2c8410af5d9ac9256
otytrackn <7149331@mail.ru>
2023-05-16 13:50:51 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,18 @@
+$(function () {
+    $("#re1").draggable();
+});
+$(function () {
+    $("#cir1").draggable();
+});
+$(function () {
+    $("#re2").draggable();
+});
+$(function () {
+    $("#re4").draggable();
+});
+$(function () {
+    $("#re8").draggable();
+});
+$(function () {
+    $("#s1").draggable();
+});
